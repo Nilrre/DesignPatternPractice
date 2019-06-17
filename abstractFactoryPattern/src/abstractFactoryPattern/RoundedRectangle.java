ac8c46deea4c94f47bc52fbd3c06bfb67d7883e9
Errlin Deleon <edeleon@localhost.localdomain>
2019-06-16 23:24:56 -0600

    Abtract Factory Added

---
+++ abstractFactoryPattern/src/abstractFactoryPattern/RoundedRectangle.java
@@ -0,0 +1,10 @@
+package abstractFactoryPattern;
+
+public class RoundedRectangle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("Rounded Rectangle has been drawn");
+	}
+
+}
